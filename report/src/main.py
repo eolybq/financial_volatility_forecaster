ac08e2e9667563c429e88ddce47b005168f95779
Václav Jež <jez.vasek@gmail.com>
2026-02-10 10:01:49 +0100

    fix page icon

--- report/src/main.py
+++ report/src/main.py
@@ -6,17 +6,18 @@ from dotenv import load_dotenv
 from services.dashboard import render_dashboard
 from services.errors import render_db_error, render_error, render_processing_error
 
+st.set_page_config(
+    page_title="Financial Volatility Forecaster Report",
+    page_icon="📈",
+    layout="wide",
+)
+
+
 load_dotenv()
 API_URL = os.getenv("API_URL")
 
 
 def main():
-    st.set_page_config(
-        page_title="Financial Volatility Forecaster Report",
-        page_icon="📈",
-        layout="wide",
-    )
-
     try:
         with st.spinner("Fetching data from Financial Volatility Forecaster API..."):
             response = requests.get(f"{API_URL}/report", timeout=10)
